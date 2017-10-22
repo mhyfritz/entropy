30ef5b60b828df267ed48e872cefcd4e37d6cd3b
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2017-10-22 11:28:39 +0200

    .

---
+++ esm/index.js
@@ -0,0 +1,15 @@
+export function entropy (str, base = 2) {
+  const counter = {}
+  for (let char of str) {
+    if (counter[char] === undefined) {
+      counter[char] = 0
+    }
+    counter[char] += 1
+  }
+  let ret = 0
+  for (const char in counter) {
+    const freq = counter[char] / str.length
+    ret += freq * (Math.log(freq) / Math.log(base))
+  }
+  return ret === 0 ? 0 : -ret
+}
